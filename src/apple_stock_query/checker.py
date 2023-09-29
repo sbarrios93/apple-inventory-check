fbbb2f978b8beed869ecbb0695db948baae56129
sbarrios93 <sbarrios93@gmail.com>
2023-09-29 14:54:04 -0400

    added more stores

--- src/apple_stock_query/checker.py
+++ src/apple_stock_query/checker.py
@@ -1,8 +1,9 @@
 import json
-import requests
-from pushsafer import Client
-from dotenv import load_dotenv
 import os
+
+import requests
+from dotenv import load_dotenv
+from pushsafer import Client
 
 try:
     load_dotenv()
@@ -12,7 +13,7 @@ except:
 
 PUSH_DEVICE = 72663
 
-SEARCH_STORES = ["R076", "R149", "R021", "R346", "R110"]
+SEARCH_STORES = ["R076", "R149", "R021", "R346", "R110", "R232", "R011", "R137", "R622"]
 
 MODELS = {
     "iPhone 15 Pro Max 256GB Natural Titanium": "MU683LL/A",
@@ -29,22 +30,22 @@ client = Client(os.environ["PUSHSAFER_PRIVATE_KEY"])
 
 
 def checker() -> None:
-
     for model, sku in MODELS.items():
         for combination_lookup in zip(SEARCH_STORES, [sku] * len(SEARCH_STORES)):
             response = requests.get(URL.format(*combination_lookup))
             data = json.loads(response.text)
             try:
                 state = data["body"]["content"]["pickupMessage"]["stores"][0][
-                "partsAvailability"
-            ][sku]["pickupDisplay"]
+                    "partsAvailability"
+                ][sku]["pickupDisplay"]
             except KeyError:
                 print(f"{model} not available in {URL.format(*combination_lookup)})")
                 continue
 
             if state == "available":
                 store_name = data["body"]["content"]["pickupMessage"]["stores"][0][
-                    "storeName"]
+                    "storeName"
+                ]
                 client.send_message(
                     f"https://www.apple.com/shop/buy-iphone/iphone-15-pro/6.7-inch-display-256gb-natural-titanium-t-mobile",
                     f"{model} available in {store_name}!",
